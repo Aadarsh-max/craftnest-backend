6c8b4a1aa81d94704b7267246dbf3c8dae9523ea
Adarsh Shrivastav <aadarshshrivastav01@gmail.com>
2026-05-23 20:04:24 +0530

    initial

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import dotenv from "dotenv";
 dotenv.config();
+
 import express from "express";
 import cors from "cors";
 
@@ -24,7 +25,13 @@ connectDB();
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:5173", process.env.CLIENT_URL],
+    credentials: true,
+  }),
+);
+
 app.use(express.json());
 
 app.use(rateLimiter);
